8d631227202b0c09f91e6f2e619966f4697cbd86
Eugene Nesvetaev <dev@nesvet.dev>
2025-07-05 12:30:56 +0400

    fix: No more `__basedir`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-//#if _SERVER
-import { basedir } from "./basedir";
-//#endif
 import { noop as _noop } from "./noop";
 import { pass as _pass } from "./pass";
 import "./Array";
@@ -11,24 +8,14 @@ import "./Object";
 import "./Set";
 
 
-type GlobalExtension = {
-	__basedir: string;
-};
-
 declare global {
-	const window: GlobalExtension & Record<string, unknown>;
-	var __basedir: string;// eslint-disable-line no-var
+	const window: Record<string, unknown>;
 	var noop: typeof _noop;// eslint-disable-line no-var
 	var pass: typeof _pass;// eslint-disable-line no-var
 }
 
 
 const _global = typeof global == "undefined" ? window : global;
-
-//#if _SERVER
-/** @deprecated */
-_global.__basedir = basedir;
-//#endif
 
 /** @deprecated */
 _global.noop = _noop;
